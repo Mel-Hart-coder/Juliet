6ff379d12e0ec489609b24f2b5e4e0c5f303c9ae
QueerInSTEM <142450309+QueerInSTEM@users.noreply.github.com>
2023-12-03 15:25:44 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+  
+    
+    
+      
+    let mainContent = document.querySelector('#main_content');
+    let secondaryContent = document.querySelector('#secondary_content');
+
+    document.querySelector('.switch input').addEventListener('change', e => { 
+        secondaryContent.style.display = e.target.checked ? 'none' : 'block';
+        mainContent.style.display = e.target.checked ? 'block' : 'none';
+    
+});
+    
+    });
